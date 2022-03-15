4c7969e743f4f94f8b6fafb49333780a5978e4d8
Chang Hoon <gelheart1011@gmail.com>
2022-03-15 16:30:40 +0900

    깃 저장소 초기화

---
+++ app.js
@@ -0,0 +1,31 @@
+"use strict";
+
+const express = require('express');
+const app = express();
+
+
+
+//라우팅
+const home = require("./routes/home");
+
+app.set('views', './views');
+app.set('view engine', 'ejs');
+
+app.use('/', home); //use -> 미들웨어를 등록
+
+module.exports = app;
+
+
+// const http = require('http');
+// const app = http.createServer((req, res)=>{
+//     res.writeHead(200, {"Content-Type":"text/html;charset=utf-8"});
+//     if( req.url === "/"){
+//         res.end('여기는 루트입니다.');
+//     }else if( req.url ==="/login"){
+//         res.end('login ');
+//     }
+// });
+
+// app.listen(3001, ()=>{
+//     console.log("TEST");
+// });
---
+++ bin/www.js
@@ -0,0 +1,7 @@
+const app = require('../app');
+
+const PORT = 3000;
+
+app.listen(PORT, function(){
+    console.log('ㅅㅓ버 가ㅇ');
+});
