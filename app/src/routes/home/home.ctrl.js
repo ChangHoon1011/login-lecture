2ae87c88e025060fdfbe10776ed0d159f09ed44d
Chang Hoon <gelheart1011@gmail.com>
2022-03-16 17:34:01 +0900

    login user add

--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,6 +1,7 @@
 "use strict";
 
-const UserStorage = require("../../models/UserStorage");
+//const UserStorage = require("../../models/UserStorage");
+const User = require("../../models/User");
 
 
 const output = {
@@ -14,26 +15,29 @@ const output = {
 
 const process = {
     login: (req, res) =>{
+        const user = new User(req.body);
+        const response = user.login();
+        return res.json(response);
        // console.log(req.body);
-       const id = req.body.id;
-       const psword = req.body.psword;
+    //    const id = req.body.id;
+    //    const psword = req.body.psword;
     
        //const userStorage = new UserStorage();
     //    console.log(UserStorage.getUsers());
     //console.log(UserStorage.getUsers("id","psword","name"));
-        const users = UserStorage.getUsers("id","psword");
+    //     const users = UserStorage.getUsers("id","psword");
 
-       const response = {};
-       if( users.id.includes(id) ){
-           const idx = users.id.indexOf(id);
-           if( users.psword[idx] === psword){
-               response.success = true;
-               return res.json(response);
-           }
-       }
-       response.success = false;
-       response.msg = "로그인애 실패하셨습니다.";
-       return res.json(response);
+    //    const response = {};
+    //    if( users.id.includes(id) ){
+    //        const idx = users.id.indexOf(id);
+    //        if( users.psword[idx] === psword){
+    //            response.success = true;
+    //            return res.json(response);
+    //        }
+    //    }
+    //    response.success = false;
+    //    response.msg = "로그인애 실패하셨습니다.";
+    //    return res.json(response);
     },
 };
 
