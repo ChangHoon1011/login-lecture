d51c232478ab3e5239d4d57a205c3e6581ec33c5
Chang Hoon <gelheart1011@gmail.com>
2022-03-16 14:03:14 +0900

    돔을 이용하여 html 객체 저장

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,4 +1,18 @@
 "use strict";
 
+const id = document.querySelector("#id"),
+    psword = document.querySelector("#psword"),
+    loginbtn = document.querySelector("button");
+
+
+loginbtn.addEventListener("click", login);
+
+function login(){
+    const req = {
+        id: id.value,
+        psword: psword.value,
+    };
+    console.log(req);
+}
+console.log(id);
 console.log("hello");
-console.log("bye");
